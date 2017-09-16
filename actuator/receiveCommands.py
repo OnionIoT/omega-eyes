b82cd9708a743e1f47cc13e4aa828ff14378521f
greenbreakfast <lazardemin@gmail.com>
2017-09-16 17:33:02 -0400

    code structure clean-up; added argument parsing to allow specifying alternate config files; added example device config

--- actuator/receiveCommands.py
+++ actuator/receiveCommands.py
@@ -1,64 +1,91 @@
-import os
-import json
+import os, sys, json, getopt
 import paho.mqtt.client as mqtt
 
 import cameraControl
 
 
-
-### MAIN PROGRAM ###
 # find the directory of the script
 dirName = os.path.dirname(os.path.abspath(__file__))
+configFile = 'config.json'
 
-# read the config file
-with open( '/'.join([dirName, 'config.json']) ) as f:
-	config = json.load(f)
-
-
-## setup mqtt
-mqttc = mqtt.Client()
-camera = cameraControl.cameraControl(0, 1, 1024)
-
-## define the mqtt callbacks
-# when connection is made
-def on_connect(client, userdata, flags, rc):
-    print("Connection result: " + str(rc))
-    # subscribe to topic specified by config file
-    mqttc.subscribe(config['topic'], 0)
-
-def on_message(client, userdata, msg):
-    if msg.payload:
-        print(msg.topic + ":: payload is " + str(msg.payload))
-        handleMessage(msg.topic, msg.payload)
-
-def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-def on_disconnect(client, userdata, rc):
-    print("Disconnected from Server")
-## end of mqtt callbacks
-
-## other functions
-def handleMessage(topic, payload):
-    if topic == config['topic']:
-        # split payload contents
-        positions = payload.split()
-        if len(positions) == 2:
-			# double check which value is which! might have been switched!
-            camera.move(int(positions[0]), int(positions[1]) )
-            print "Received positions (%d, %d)"%(int(positions[0]), int(positions[1]))
-        else:
-            print("Invalid number of arguments received")
+# read the command line arguments
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "config="])
+except getopt.GetoptError:
+	printUsage()
+	sys.exit(2)
+for opt, arg, in opts:
+	if opt in ("-h", "--help"):
+		printUsage()
+		sys.exit()
+	elif opt in ("-c", "--config"):
+		configFile = arg
 
 
-## Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_subscribe = on_subscribe
-mqttc.on_disconnect = on_disconnect
-# Connect
-mqttc.connect(config['server'], config['port'], 60)
+### MAIN PROGRAM ###
+def __main__():
+	# read the config file
+	configPath = '/'.join([dirName, configFile])
+	with open( configPath ) as f:
+		try:
+			config = json.load(f)
+		except:
+			print("ERROR: expecting JSON file at '%s'"%configPath)
+			sys.exit()
 
 
-# Continue the network loop
-mqttc.loop_forever()
+	## setup mqtt
+	mqttc = mqtt.Client()
+	try:
+		camera = cameraControl.cameraControl(config['panChannel'], config['tiltChannel'], config['inputRange'])
+	except:
+		print("ERROR: config file most contain settings for 'panChannel', 'tiltChannel', and 'inputRange'")
+
+	## define the mqtt callbacks
+	# when connection is made
+	def on_connect(client, userdata, flags, rc):
+	    print("Connection result: " + str(rc))
+	    # subscribe to topic specified by config file
+	    mqttc.subscribe(config['topic'], 0)
+
+	def on_message(client, userdata, msg):
+	    if msg.payload:
+	        print(msg.topic + ":: payload is " + str(msg.payload))
+	        handleMessage(msg.topic, msg.payload)
+
+	def on_subscribe(client, userdata, mid, granted_qos):
+	    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+	def on_disconnect(client, userdata, rc):
+	    print("Disconnected from Server")
+	## end of mqtt callbacks
+
+	## other functions
+	def handleMessage(topic, payload):
+	    if topic == config['topic']:
+	        # split payload contents
+	        positions = payload.split()
+	        if len(positions) == 2:
+				# double check which value is which! might have been switched!
+	            camera.move(int(positions[0]), int(positions[1]) )
+	            print "Received positions (%d, %d)"%(int(positions[0]), int(positions[1]))
+	        else:
+	            print("Invalid number of arguments received")
+
+
+	## Assign event callbacks
+	mqttc.on_message = on_message
+	mqttc.on_connect = on_connect
+	mqttc.on_subscribe = on_subscribe
+	mqttc.on_disconnect = on_disconnect
+	# Connect
+	mqttc.connect(config['server'], config['port'], 60)
+
+
+	# Continue the network loop
+	mqttc.loop_forever()
+
+
+
+if __name__ == '__main__':
+    __main__()
